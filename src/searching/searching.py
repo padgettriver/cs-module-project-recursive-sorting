d0329f3a44ec1b789c6b317281464469260f515c
padgettriver <padgettcriver@gmail.com>
2020-07-22 18:43:29 -0400

    MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,18 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    if start <= end:
+        total = start + end
+        half = total // 2
+        if arr[half] is target:
+            return half
+        elif arr[half] < target:
+            return binary_search(arr, target, half + 1, end)
+        else:
+            return binary_search(arr, target, 0, half - 1)
+    else:
+        return -1
+
+
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -9,6 +21,7 @@ def binary_search(arr, target, start, end):
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
-def agnostic_binary_search(arr, target):
-    # Your code here
-
+#-------------------------------
+# def agnostic_binary_search(arr, target):
+#     # Your code here
+#     pass
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -4,14 +4,36 @@ def merge(arrA, arrB):
     merged_arr = [0] * elements
 
     # Your code here
+    a_counter = 0
+    b_counter = 0
 
-
+    for i in range(0, elements):
+        if a_counter == len(arrA) or b_counter == len(arrB):   # if one side runs out of numbers
+            if a_counter == len(arrA):
+                merged_arr[i] = arrB[b_counter]
+                b_counter += 1
+            else:
+                merged_arr[i] = arrA[a_counter]
+                a_counter += 1
+        else:                                                 # compare and add lowest number
+            if arrA[a_counter] < arrB[b_counter]:
+                merged_arr[i] = arrA[a_counter]
+                a_counter += 1
+            else:
+                merged_arr[i] = arrB[b_counter]
+                b_counter += 1
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # Your code here
-
+    start = 0
+    end = len(arr)
+    if end > start + 1:
+        mid = int((start + end) / 2)
+        arrA = merge_sort(arr[0:mid])
+        arrB = merge_sort(arr[mid:end])
+        arr = merge(arrA, arrB)
 
     return arr
 
@@ -21,8 +43,8 @@ def merge_sort(arr):
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     # Your code here
-
+    pass
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
-
+    pass
